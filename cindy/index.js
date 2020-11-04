5992e94f9a65ff54e87c42a318a680e1c0a67ca8
Cindy <Punk.c@hotmail.com>
2020-11-04 10:54:30 +0100

    add task 2 and 3

--- cindy/index.js
+++ cindy/index.js
@@ -20,9 +20,22 @@ const student = {
     },
   };
   console.log("student ==> ", student.getObjectValues());
+ 
 
 //     Create a person object. Include the person's first and last name, age, job, city etc. Create a method to print data from the object e.g. "John Smith is a 41 year old engineer living in France".
-
+const student2 = {
+  firstName: "John",
+  lastName: "Smith",
+  age: 12, 
+  job: "student",
+  class: 12,
+  city: "Paris",
+  getObjectValues: function () {
+    return `${this.firstName} ${this.lastName} is a ${this.age} year old ${this.job} in ${this.city} in class ${this.class} `;
+  },
+};
+console.log("Task 2  ==> ", student2.getObjectValues());
 // Bonus
 
-//     Write a method to get the length of the person object
+//     Write a method to get the length of the person object=> pending! 
+console.log("students length! => ", Object.keys(student))//Done it <= 
