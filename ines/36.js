44cecad719b1be8236435241e5da928664d2df43
INes <inesfcnogueira@gmail.com>
2020-11-11 11:15:00 +0100

    Updated with requested changes

--- ines/36.js
+++ ines/36.js
@@ -4,7 +4,9 @@ const student = {
     lastName: "Smith", 
     class: 12 };
 
-console.log(`${student.firstName} ${student.lastName} is a student in class ${student.class}`);
+const print = (object) =>  `${object.firstName} ${object.lastName} is a student in class ${object.class}`
+
+console.log(print(student));
 
 //2
 const person = {
@@ -14,7 +16,11 @@ const person = {
     job: "Web Developer",
     city: "Berlin",
 }
-console.log(`${person.firstName} ${person.lastName} is ${person.age} years old ${person.job} based in ${person.city}`);
+
+const print2  = (object) =>  `${object.firstName} ${object.lastName} is ${object.age} years old ${object.job} based in ${object.city}`
+
+console.log(print2(person));
+
 
 //3
 console.log(Object.keys(person).length);
